36f6aada5d6a10967849d7156f2b9baecac9cf15
Andrew Souza <souzaphonix@pm.me>
2026-01-17 01:04:24 -0800

    Add bitfield and havePieces checks. Send interested message, receive unchoke, and send a request message.

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 /*
-gotorrent
-by Andrew Souza
-GPLv3
-*/
+ *
+ * title:   gotorrent
+ * author:  Andrew Souza
+ * license: GPLv3
+ *
+ */
 package main
 
 import (
 	"fmt"
-	"log"
 	"math/rand/v2"
 	"time"
 
-	"io"
 	"net"
 	"net/url"
 	"os"
@@ -25,13 +25,13 @@ func main() {
 	args := os.Args
 	rawBytes, err := os.ReadFile(args[1])
 	if err != nil {
-		fmt.Printf("failed to open torrent file: $v\n", err)
+		fmt.Printf("failed to open torrent file: %v\n", err)
 		os.Exit(1)
 	}
 
 	tor, err := torrent.DecodeTorrentFile(rawBytes)
 	if err != nil {
-		fmt.Errorf("failed to decode torrent file: $v\n", err)
+		fmt.Printf("failed to decode torrent file: %v\n", err)
 		os.Exit(1)
 	}
 
@@ -42,13 +42,36 @@ func main() {
 
 	ur, err := url.Parse(announce)
 	if err != nil {
-		fmt.Errorf("invalid announce url: $v\n", err)
-		os.Exit(1)
+		fmt.Printf("invalid announce url: %v\n", err)
 	}
 
 	rs, err := req.Announce(ur)
 	if err != nil {
-		fmt.Errorf("Announce failure: $v\n", err)
+		fmt.Printf("Announce failure: %v\n", err)
+	}
+
+	if len(rs.Peers) == 0 && len(tor.Announce) > 1 {
+		for _, announce := range tor.Announce[1:] {
+			ur, err = url.Parse(announce)
+			if err != nil {
+				fmt.Printf("invalid announce url: %v\n", err)
+				continue
+			}
+
+			rs, err = req.Announce(ur)
+			if err != nil {
+				fmt.Printf("Announce failure: %v\n", err)
+				continue
+			}
+
+			if len(rs.Peers) > 0 {
+				break
+			}
+		}
+	}
+
+	if len(rs.Peers) == 0 {
+		fmt.Printf("no peers found\n")
 		os.Exit(1)
 	}
 
@@ -58,44 +81,28 @@ func main() {
 	fmt.Printf("Seeders: %d, Leechers: %d\n", rs.Seeders, rs.Leechers)
 
 	randomIndex := uint32(rand.IntN(len(rs.Peers)))
-
-	fmt.Printf("rs.Peers[%d].IP: $v\n\n", randomIndex, rs.Peers[randomIndex].IP)
-	fmt.Printf("rs.Peers[%d].Port: $v\n\n", randomIndex, rs.Peers[randomIndex].Port)
+	rs.PrintPeerInfo(randomIndex)
 
 	handshake := peer_protocol.NewHandshake(req.InfoHash, req.Peer.ID)
-	handshakeMsg := handshake.Serialize()
-	fmt.Printf("handshakeMsg: $v\n\n", handshakeMsg)
-
-	peerHandshake := make([]byte, 68)
 
 	conn, err := net.DialTimeout("tcp", rs.Peers[randomIndex].Address(), time.Second*10)
 	if err != nil {
-		fmt.Errorf("tcp connection failure: $v\n", err)
+		fmt.Printf("tcp connection failure: %v\n", err)
 		os.Exit(1)
 	}
 	defer conn.Close()
 
-	_, err = conn.Write(handshakeMsg)
-	if err != nil {
-		fmt.Errorf("failed to write handshake: $v\n", err)
-		os.Exit(1)
-	}
-
-	_, err = io.ReadFull(conn, peerHandshake)
-	if err != nil {
-		fmt.Errorf("failed to receive handshake from peer: $v\n", err)
-		os.Exit(1)
-	}
-
-	theirPeerID, err := peer_protocol.ValidateHandshake(peerHandshake, handshake.InfoHash)
-	if err != nil {
-		fmt.Errorf("invalid handshake: %\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("theirPeerID: $v\n\n", theirPeerID)
+	theirPeerID, err := handshake.FetchPeer(conn)
 
 	connectedState := peer_protocol.NewPeerConn(conn, theirPeerID, tor)
 
-	fmt.Printf("connectedState: %v\n", connectedState)
+	for {
+		msg, err := connectedState.ReadMsg()
+		if err != nil {
+			fmt.Printf("ReadMsg failure: %v\n", err)
+			break
+		}
+
+		fmt.Printf("msg sent: %v\n", msg)
+	}
 }
--- pkg/peer_protocol/peer_protocol.go
+++ pkg/peer_protocol/peer_protocol.go
@@ -11,15 +11,16 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 
 	"github.com/drewslam/gotorrent/pkg/torrent"
 )
 
-type MsgVal uint8
+type MsgIndex uint8
 
 const (
-	Choke MsgVal = iota
+	Choke MsgIndex = iota
 	Unchoke
 	Interested
 	NotInterested
@@ -28,8 +29,10 @@ const (
 	Request
 	Piece
 	Cancel
-	MaxMsgVal = Cancel
+	MaxMsgIndex = Cancel
 )
+
+const MaxBlockSize uint32 = 16384
 
 type PeerState struct {
 	Choking    bool
@@ -46,18 +49,20 @@ type Handshake struct {
 
 type Message struct {
 	Length  uint32
-	ID      MsgVal
+	ID      MsgIndex
 	Payload []byte
 }
 
 type PeerConn struct {
-	Conn net.Conn
+	Conn   net.Conn
 	PeerID [20]byte
 
 	ClientState *PeerState
 	PeerState   *PeerState
 
-	Bitfield []byte
+	NumPieces  uint32
+	Bitfield   []byte
+	HavePieces []byte
 }
 
 func NewPeerState() *PeerState {
@@ -80,7 +85,7 @@ func NewHandshake(info [20]byte, peerID [20]byte) *Handshake {
 }
 
 func NewPeerConn(conn net.Conn, peerID [20]byte, tor *torrent.Torrent) *PeerConn {
-	numPieces := len(tor.Info.Pieces)
+	numPieces := uint32(len(tor.Info.Pieces))
 	bitfieldSize := (numPieces + 7) / 8
 
 	return &PeerConn{
@@ -88,11 +93,33 @@ func NewPeerConn(conn net.Conn, peerID [20]byte, tor *torrent.Torrent) *PeerConn
 		PeerID:      peerID,
 		ClientState: NewPeerState(),
 		PeerState:   NewPeerState(),
+		NumPieces:   numPieces,
 		Bitfield:    make([]byte, bitfieldSize),
+		HavePieces:  make([]byte, bitfieldSize),
 	}
 }
 
-func (h *Handshake) Serialize() []byte {
+func (h *Handshake) FetchPeer(conn net.Conn) ([20]byte, error) {
+	peerHandshake := make([]byte, 68)
+	_, err := conn.Write(h.serialize())
+	if err != nil {
+		return [20]byte{}, fmt.Errorf("failed to write handshake: %v", err)
+	}
+
+	_, err = io.ReadFull(conn, peerHandshake)
+	if err != nil {
+		return [20]byte{}, fmt.Errorf("failed to receive handshake from peer: %v", err)
+	}
+
+	theirPeerID, err := validateHandshake(peerHandshake, h.InfoHash)
+	if err != nil {
+		return [20]byte{}, fmt.Errorf("invalid handshake: %v", err)
+	}
+
+	return theirPeerID, nil
+}
+
+func (h *Handshake) serialize() []byte {
 	buffer := bytes.NewBuffer([]byte{h.Pstrlen})
 	buffer.Write(h.Pstr[:])
 	buffer.Write(h.Reserved[:])
@@ -116,7 +143,7 @@ func (m *Message) Serialize() []byte {
 	return buffer.Bytes()
 }
 
-func ValidateHandshake(input []byte, expectedInfoHash [20]byte) ([20]byte, error) {
+func validateHandshake(input []byte, expectedInfoHash [20]byte) ([20]byte, error) {
 	if len(input) != 68 {
 		return [20]byte{0}, fmt.Errorf("incorrect length handshake")
 	}
@@ -138,4 +165,105 @@ func ValidateHandshake(input []byte, expectedInfoHash [20]byte) ([20]byte, error
 	copy(peerID[:], input[48:])
 
 	return peerID, nil
+}
+
+func (p *PeerConn) ReadMsg() (*Message, error) {
+	lenBuffer := make([]byte, 4)
+	_, err := io.ReadFull(p.Conn, lenBuffer)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read message length: %w", err)
+	}
+	lenPrefix := binary.BigEndian.Uint32(lenBuffer[:])
+
+	if lenPrefix != 0 {
+		msgBuf := make([]byte, lenPrefix)
+		_, err = io.ReadFull(p.Conn, msgBuf)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse message body: %w", err)
+		}
+
+		fmt.Printf("msg received: %v\n", msgBuf)
+
+		msgIndex := MsgIndex(msgBuf[0])
+
+		switch msgIndex {
+		case Choke:
+			p.PeerState.Choking = true
+		case Unchoke:
+			p.PeerState.Choking = false
+			// select appropriate piece index
+			pieceIndex := -1
+			for i := range p.NumPieces {
+				if p.PeerHasPiece(i) && !p.WeHavePiece(i) {
+					pieceIndex = int(i)
+					break
+				}
+			}
+			if pieceIndex == -1 {
+				return nil, nil
+			}
+			// format request message
+			return p.PrepareRequest(uint32(pieceIndex), 0)
+		case Interested:
+			p.PeerState.Interested = true
+		case NotInterested:
+			p.PeerState.Interested = false
+		case Have:
+			fmt.Printf("have\n")
+		case Bitfield:
+			p.Bitfield = msgBuf[1:]
+			p.ClientState.Interested = true
+			return &Message{
+				Length: 1,
+				ID:     Interested,
+			}, nil
+		case Request:
+			fmt.Printf("request\n")
+		case Piece:
+			pieceIndex := binary.BigEndian.Uint32(msgBuf[1:5])
+			offset := binary.BigEndian.Uint32(msgBuf[5:9])
+			blockData := msgBuf[9:]
+
+			fmt.Printf("piece: %d\noffset: %d\n%d bytes\n", pieceIndex, offset, len(blockData))
+			// TODO verify hash and save data
+		case Cancel:
+			fmt.Printf("cancel\n")
+		default:
+			fmt.Printf("unrecognized message ID: %d\n", msgIndex)
+		}
+
+	}
+
+	return nil, nil
+}
+
+func (p *PeerConn) PrepareRequest(index uint32, offset uint32) (*Message, error) {
+	payload := make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], index)
+	binary.BigEndian.PutUint32(payload[4:8], offset)
+	binary.BigEndian.PutUint32(payload[8:12], MaxBlockSize)
+
+	return &Message{
+		Length: 13,
+		ID: Request,
+		Payload: payload,
+	}, nil
+}
+
+func (p *PeerConn) PeerHasPiece(index uint32) bool {
+	byteIndex := index / 8
+	bitIndex := 7 - (index % 8)
+	if byteIndex >= uint32(len(p.Bitfield)) {
+		return false
+	}
+	return (p.Bitfield[byteIndex] & (1 << bitIndex)) != 0
+}
+
+func (p *PeerConn) WeHavePiece(index uint32) bool {
+	byteIndex := index / 8
+	bitIndex := (7 - (index % 8))
+	if byteIndex >= uint32(len(p.HavePieces)) {
+		return false
+	}
+	return (p.HavePieces[byteIndex] & (1 << bitIndex)) != 0
 }
