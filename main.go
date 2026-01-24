820c08a6bb68bd4a8a477b98f9fde28d2c7f1dd8
Andrew Souza <souzaphonix@pm.me>
2026-01-24 11:17:51 -0800

    Add ActiveConnections handler map to prevent collisions when calling random peers.

--- main.go
+++ main.go
@@ -10,9 +10,7 @@ package main
 import (
 	"fmt"
 	"math/rand/v2"
-	"time"
 
-	"net"
 	"net/url"
 	"os"
 
@@ -22,6 +20,7 @@ import (
 )
 
 const MaxPeerConnections uint32 = 5
+var ActivePeerConnections = make(map[uint32]bool, MaxPeerConnections)
 
 func main() {
 	args := os.Args
@@ -85,57 +84,34 @@ func main() {
 	pieceMgr := peer_protocol.NewPieceManager(tor)
 
 	for range MaxPeerConnections {
-		randomIndex := uint32(rand.IntN(len(rs.Peers)))
+		if len(ActivePeerConnections) > len(rs.Peers) {
+			fmt.Printf("only %d peers available (wanted %d)\n", len(rs.Peers), MaxPeerConnections)
+		}
+
+		var randomIndex uint32
+		maxAttempts := len(rs.Peers) * 2
+		attempts := 0
+
+		for {
+		randomIndex = uint32(rand.IntN(len(rs.Peers)))
+			if _, exists := ActivePeerConnections[randomIndex]; !exists {
+				break
+			}
+
+			attempts++
+			if attempts > maxAttempts {
+				fmt.Printf("failed to find unique peer after %d attempts\n", attempts)
+				break
+			}
+		}
+
 		rs.PrintPeerInfo(randomIndex)
+		ActivePeerConnections[randomIndex] = true
+
 		go func(peerAddr string, peerIdx uint32) {
-			HandleConnection(peerAddr, peerIdx, req, pieceMgr)
+			pieceMgr.HandleConnection(peerAddr, peerIdx, req)
 		}(rs.Peers[randomIndex].Address(), randomIndex)
 	}
 
 	select {}
-}
-
-func HandleConnection(peerAddr string, peerIdx uint32, req *tracker.Request, pieceMgr *peer_protocol.PieceManager) {
-	handshake := peer_protocol.NewHandshake(req.InfoHash, req.Peer.ID)
-
-	conn, err := net.DialTimeout("tcp", peerAddr, time.Second*10)
-	if err != nil {
-		fmt.Printf("tcp connection failure: %v\n", err)
-		return
-	}
-	defer conn.Close()
-
-	theirPeerID, err := handshake.FetchPeer(conn)
-	if err != nil {
-		fmt.Printf("peer %d handshake failed: %v\n", peerIdx, err)
-	}
-
-	connectedState := peer_protocol.NewPeerConn(conn, theirPeerID, pieceMgr)
-
-	// keep alive
-	ticker := time.NewTicker(time.Minute * 2)
-	defer ticker.Stop()
-
-	go func() {
-		for range ticker.C {
-			keepAlive := &peer_protocol.Message{Length: 0}
-			if _, err := conn.Write(keepAlive.Serialize()); err != nil {
-				return
-			}
-		}
-	}()
-
-	for {
-		msg, err := connectedState.ReadMsg()
-		if err != nil {
-			fmt.Printf("peer %d read error: %v\n", peerIdx, err)
-			return
-		}
-
-		err = connectedState.WriteMsgResponse(msg)
-		if err != nil {
-			fmt.Printf("peer %d write error: %v\n", peerIdx, err)
-			return
-		}
-	}
 }
